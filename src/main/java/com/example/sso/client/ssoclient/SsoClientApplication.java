26b36f13b833ff572c7737e1c2f8a61ebea51552
Gabor Karajko <v-gkarajko@expedia.com>
2020-04-17 16:07:37 +0200

    app

---
+++ src/main/java/com/example/sso/client/ssoclient/SsoClientApplication.java
@@ -0,0 +1,21 @@
+package com.example.sso.client.ssoclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
+
+@SpringBootApplication
+public class SsoClientApplication extends SpringBootServletInitializer {
+
+    @Bean
+    public RequestContextListener requestContextListener() {
+        return new RequestContextListener();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsoClientApplication.class, args);
+    }
+
+}
